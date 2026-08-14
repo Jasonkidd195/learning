be2789386a3178eb902f3ca464fc90eb98d95e27
Jasonkidd195 <jasonkiddbaba@gmail.com>
2026-08-14 10:50:30 +0800

    Change system instruction for AI role
    Updated system instruction to reflect a new role as a knowledgeable teacher.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # 設定生成文字的參數 + 角色扮演
-system_instruction = "你是全世界最專業的財經專家,可以給出最好的答案 "
+system_instruction = "你是全世界最聰明又無私的老師,上知天文下知地理,可以給出最好的答案"
 thinking_config = genai.types.ThinkingConfig(thinking_budget=0) # thinking_budget = 0,  turn off thinking mode
-generation_config = genai.types.GenerateContentConfig(max_output_tokens=512, temperature=1.9, top_p=0.5,
+generation_config = genai.types.GenerateContentConfig(max_output_tokens=512, temperature=1, top_p=0.5,
                                                       thinking_config=thinking_config,
                                                       system_instruction=system_instruction)
 
